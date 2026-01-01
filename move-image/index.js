3c7c09f183f3fb9663d33ac08f0077424db46643
Shivam-Gupta-git <shivamgupta73014@gmail.com>
2026-01-01 18:16:46 +0530

    move image

--- move-image/index.js
+++ move-image/index.js
@@ -3,21 +3,20 @@ const boxes = document.querySelectorAll(".box");
 boxes.forEach((box) => {
     const img = box.querySelector("img");
 
-    // Set background from data-bg attribute
+
     const bg = box.getAttribute("data-bg");
     box.style.setProperty('--bg-image', `url(${bg})`);
-    box.style.backgroundImage = `url(${bg})`; // fallback if you want
+    box.style.backgroundImage = `url(${bg})`; 
 
     box.addEventListener("mouseenter", () => {
         img.style.opacity = "1";
-        // also show background via ::before
         box.style.backgroundImage = `url(${bg})`;
         box.classList.add('bg-visible');
     });
 
     box.addEventListener("mouseleave", () => {
         img.style.opacity = "0";
-        box.style.backgroundImage = ''; // remove background
+        box.style.backgroundImage = '';
         box.classList.remove('bg-visible');
     });
 
